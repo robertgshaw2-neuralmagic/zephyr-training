0f475f8624b7822368707aae51550c4bbfd439c4
rsnm2 <rshaw@neuralmagic.com>
2023-11-11 16:42:01 +0000

    updated script

--- launch_training_docker.sh
+++ launch_training_docker.sh
@@ -5,5 +5,5 @@ cd training
 docker run \
     --shm-size 2gb --gpus all \
     -v $PWD/data:/data -v $PWD/scripts:/scripts \
-    -e HF_TOKEN=$HF_TOKEN_READ -e HF_TOKEN_WRITE=$HF_TOKEN_WRITE -e HF_HOME="/data" -e WANDB_API_KEY=$WANDB_API_KEY \
+    -e HUGGING_FACE_HUB_TOKEN=$HF_TOKEN_READ -e HF_TOKEN_WRITE=$HF_TOKEN_WRITE -e HF_HOME="/data" -e WANDB_API_KEY=$WANDB_API_KEY \
     --network host -it --rm llmf
