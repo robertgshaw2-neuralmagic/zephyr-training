1ad79b53cba3c7163927ad92d23905dcdef0d4cb
rsnm2 <rshaw@neuralmagic.com>
2023-11-11 16:49:16 +0000

    fixed bug

--- training/scripts/train.py
+++ training/scripts/train.py
@@ -610,6 +610,15 @@ def main(cfg: DictConfig) -> Trainer:
                 tokenizer)
             print_trainable_parameters(model)  # should not be 100%
         else:  # standard model
+            # initialize teacher model
+           if knowledge_distillation_config is not None:
+                print(f"[Debugging] Knowledge Distillation config = {knowledge_distillation_config}")
+                teacher_config = copy.deepcopy(model_config)
+                teacher_config['pretrained_model_name_or_path'] = knowledge_distillation_config.teacher_name_or_path
+                teacher = build_composer_model(teacher_config, tokenizer)
+                teacher.eval()
+                teacher = teacher.to(torch.bfloat16)
+                
             model = build_composer_model(model_config, tokenizer)
 
         if model_config.get('master_weights_dtype') in ('bf16', 'bfloat16'):
@@ -618,14 +627,7 @@ def main(cfg: DictConfig) -> Trainer:
             model = model.to(dtype=torch.float16)
 
 ################################################################################################################################################################
-        # initialize teacher model
-        if knowledge_distillation_config is not None:
-            print(f"[Debugging] Knowledge Distillation config = {knowledge_distillation_config}")
-            teacher_config = copy.deepcopy(model_config)
-            teacher_config['pretrained_model_name_or_path'] = cfg.knowledge_distillation.teacher_name_or_path
-            teacher = build_composer_model(teacher_config, tokenizer)
-            teacher.eval()
-
+        
     if sparse_finetuning:
         # 1) Attach Sparsity Masks
         attach_masks(model, torch.nn.Linear)
